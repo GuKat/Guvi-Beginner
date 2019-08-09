56f09ed4c3f1c93482d182e17f3089f6f4240514
Gugan A R <36396356+GuKat@users.noreply.github.com>
2019-08-09 23:26:13 +0530

    Create Odd or Even.py

---
+++ Odd or Even.py
@@ -0,0 +1,8 @@
+a = input("Enter the input:")
+if a.isdigit():
+    if(a%2==0):
+        print("Number is Even")  
+    else:  
+        print("Number is Odd") 
+else:
+    print("Ivalid Character")
